fa519cb3b5d6bad1caa5a71d7a2abebbd63d07fa
Mario Fronza <mariofronza1@gmail.com>
2022-08-23 21:13:29 -0300

    update serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ val koin_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.serialization") version "1.6.21"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 }
 
 
